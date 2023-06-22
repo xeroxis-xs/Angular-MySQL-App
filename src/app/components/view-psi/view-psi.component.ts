616cd79dcce0adaf05b45220eb5a8cee47003f6e
Teoh Xi Sheng <txsheng0609@hotmail.com>
2023-06-22 11:52:34 +0800

    Updated view-psi component

--- src/app/components/view-psi/view-psi.component.ts
+++ src/app/components/view-psi/view-psi.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PSIService } from 'src/app/services/psi.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-psi',
   templateUrl: './view-psi.component.html',
-  styleUrls: ['./view-psi.component.scss']
+  styleUrls: ['./view-psi.component.scss'],
 })
-export class ViewPsiComponent {
+export class ViewPsiComponent implements OnInit {
+  psi: any;
 
+  constructor(
+    private psiService: PSIService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.loadATemp();
+  }
+
+  loadATemp() {
+    const id = this.route.snapshot.params['id'];
+    this.psiService.getAPSI(id).subscribe({
+      next: (response) => {
+        this.psi = response;
+        console.log(this.psi);
+      },
+      error: (err) => console.error(err),
+    });
+  }
 }
