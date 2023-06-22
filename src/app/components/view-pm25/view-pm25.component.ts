19fc3d76126d7b5c980292cb75ca336b64c96c41
Teoh Xi Sheng <txsheng0609@hotmail.com>
2023-06-22 11:53:02 +0800

    Update view-pm25 component

--- src/app/components/view-pm25/view-pm25.component.ts
+++ src/app/components/view-pm25/view-pm25.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PM25Service } from 'src/app/services/pm25.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-pm25',
   templateUrl: './view-pm25.component.html',
-  styleUrls: ['./view-pm25.component.scss']
+  styleUrls: ['./view-pm25.component.scss'],
 })
-export class ViewPm25Component {
+export class ViewPm25Component implements OnInit {
+  pm25: any;
 
+  constructor(
+    private pm25Service: PM25Service,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.loadATemp();
+  }
+
+  loadATemp() {
+    const id = this.route.snapshot.params['id'];
+    this.pm25Service.getAPM25(id).subscribe({
+      next: (response) => {
+        this.pm25 = response;
+        console.log(this.pm25);
+      },
+      error: (err) => console.error(err),
+    });
+  }
 }
