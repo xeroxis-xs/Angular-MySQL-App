8a7cddbb59c1b557ca6bcc5845e989766b9281c6
Teoh Xi Sheng <txsheng0609@hotmail.com>
2023-06-22 11:51:50 +0800

    Updated view-temp component

--- src/app/components/view-temp/view-temp.component.ts
+++ src/app/components/view-temp/view-temp.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TempService } from 'src/app/services/temp.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-temp',
   templateUrl: './view-temp.component.html',
-  styleUrls: ['./view-temp.component.scss']
+  styleUrls: ['./view-temp.component.scss'],
 })
-export class ViewTempComponent {
+export class ViewTempComponent implements OnInit {
+  temp: any;
 
+  constructor(
+    private tempService: TempService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.loadATemp();
+  }
+
+  loadATemp() {
+    const id = this.route.snapshot.params['id'];
+    this.tempService.getATemp(id).subscribe({
+      next: (response) => {
+        this.temp = response;
+        console.log(this.temp);
+      },
+      error: (err) => console.error(err),
+    });
+  }
 }
