523c73c9b6125f08608d1506192ba9e89ebc23b3
Teoh Xi Sheng <txsheng0609@hotmail.com>
2023-06-22 11:53:18 +0800

    Update view-humid component

--- src/app/components/view-humid/view-humid.component.ts
+++ src/app/components/view-humid/view-humid.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HumidService } from 'src/app/services/humid.service';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-view-humid',
   templateUrl: './view-humid.component.html',
-  styleUrls: ['./view-humid.component.scss']
+  styleUrls: ['./view-humid.component.scss'],
 })
-export class ViewHumidComponent {
+export class ViewHumidComponent implements OnInit {
+  humid: any;
 
+  constructor(
+    private humidService: HumidService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.loadAHumid();
+  }
+
+  loadAHumid() {
+    const id = this.route.snapshot.params['id'];
+    this.humidService.getAHumid(id).subscribe({
+      next: (response) => {
+        this.humid = response;
+        console.log(this.humid);
+      },
+      error: (err) => console.error(err),
+    });
+  }
 }
