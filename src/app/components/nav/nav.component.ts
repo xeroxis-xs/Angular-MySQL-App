4a639b908e64e3c097bf97270586a6d1b5f97ba7
Teoh Xi Sheng <txsheng0609@hotmail.com>
2023-06-28 09:25:29 +0800

    Update side nav such that it includes building info

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { LevelService } from 'src/app/services/level.service';
+import { LevelModel } from 'src/app/models/level.model';
+import { BuildingModel } from 'src/app/models/building.model';
+import { BuildingService } from 'src/app/services/building.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  levels: LevelModel[];
+  buildings: BuildingModel[];
 
+  constructor(
+    private levelService: LevelService,
+    private buildingService: BuildingService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadBuilding();
+    this.loadLevel();
+  }
+
+  loadBuilding() {
+    this.buildingService.getAllBuilding().subscribe({
+      next: (response) => {
+        this.buildings = response;
+        // console.log(this.levels);
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  loadLevel() {
+    this.levelService.getAllLevel().subscribe({
+      next: (response) => {
+        this.levels = response;
+        // console.log(this.levels);
+      },
+      error: (err) => console.error(err),
+    });
+  }
 }
